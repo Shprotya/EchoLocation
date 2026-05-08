943b2f2260f253d2e4faf056dd4014b5c02316e2
Lisa <kareevaliza@gmail.com>
2026-05-08 16:59:15 +0100

    populated sidebar component

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, effect } from '@angular/core';
+import { Track } from '../../models/track.model';
+import { TrackList } from '../track-list/track-list';
+import { Countryservice } from '../../services/countryservice';
+import { LastfmService } from '../../services/lastfmservice';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [TrackList],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css',
+  styleUrl: './sidebar.css'
 })
 export class Sidebar {
+  private countryService = inject(Countryservice);
+  private lastfmService = inject(LastfmService);
 
+  selectedCountry = this.countryService.selectedCountry;
+  tracks: Track[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor() {
+    effect(() => {
+      const country = this.selectedCountry();
+      if (country) {
+        this.fetchTracks(country);
+      }
+    });
+  }
+
+  private fetchTracks(country: string): void {
+    this.loading = true;
+    this.error = null;
+    this.lastfmService.getTopTracks(country).subscribe({
+      next: tracks => {
+        this.tracks = tracks;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Could not load tracks for this country.';
+        this.loading = false;
+      }
+    });
+  }
 }
