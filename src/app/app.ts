035728fc3a817449d44481320cfc8340f5eccff7
Lisa <kareevaliza@gmail.com>
2026-05-08 17:04:39 +0100

    wire up sidebar to application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Map } from "./components/map/map";
+import { Sidebar } from "./components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [Map],
+  imports: [Map, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
