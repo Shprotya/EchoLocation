341874c1442f929b390c1bed93754dd16d7f28e0
Lisa <kareevaliza@gmail.com>
2026-05-08 17:01:23 +0100

    track list and track item set

--- src/app/components/track-item/track-item.ts
+++ src/app/components/track-item/track-item.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Track } from '../../models/track.model';
 
 @Component({
   selector: 'app-track-item',
   imports: [],
-  templateUrl: './track-item.html',
-  styleUrl: './track-item.css',
+  template: `
+    <li class="bg-gray-800 rounded p-3">
+      <a [href]="track().url" target="_blank" class="font-medium hover:text-blue-400">
+        {{ track().name }}
+      </a>
+      <p class="text-sm text-gray-400">{{ track().artist.name }}</p>
+      <p class="text-xs text-gray-500">{{ track().listeners }} listeners</p>
+    </li>
+  `
 })
 export class TrackItem {
-
+  track = input.required<Track>();
 }
--- src/app/components/track-list/track-list.ts
+++ src/app/components/track-list/track-list.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { TrackItem } from '../track-item/track-item';
+import { input } from '@angular/core';
+import { Track } from '../../models/track.model';
 
 @Component({
   selector: 'app-track-list',
-  imports: [],
-  templateUrl: './track-list.html',
-  styleUrl: './track-list.css',
+  imports: [TrackItem],
+  template: `
+    <ol class="space-y-2">
+      @for (track of tracks(); track track.name) {
+        <app-track-item [track]="track" />
+      }
+    </ol>
+  `
 })
 export class TrackList {
-
+  tracks = input.required<Track[]>();
 }
