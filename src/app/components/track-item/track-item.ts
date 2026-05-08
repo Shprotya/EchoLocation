ddfe751e139d8f0dd47438ebe945ae8617f1ed08
Lisa <kareevaliza@gmail.com>
2026-05-08 16:44:47 +0100

    generated components and moved map component

---
+++ src/app/components/track-item/track-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-item',
+  imports: [],
+  templateUrl: './track-item.html',
+  styleUrl: './track-item.css',
+})
+export class TrackItem {
+
+}
